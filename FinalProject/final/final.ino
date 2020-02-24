38b87964f8537e411515829828fe39a1df47910b
Miruna <36522518+miruna1224@users.noreply.github.com>
2020-02-24 18:50:07 +0200

    Update final.ino

--- FinalProject/final/final.ino
+++ FinalProject/final/final.ino
@@ -1,8 +1,9 @@
-
+// arduino common pins
 int datapin=2;
 int clockpin=3;
 int latchpin=4;
 
+// colors for rgb in binary
 byte ledB = B11011111;
 byte ledG = B10111111;
 byte ledR = B01111111;
@@ -12,24 +13,28 @@ byte ledT = B10011111;
 byte ledW = B00011111;
 byte led0 = B11111111;
 
-byte leds[] = {ledR, ledR, ledR, ledR, ledR, ledR, ledR, ledR, ledR, 
-               ledW, ledW, ledW, ledW, ledW, ledW, ledW, ledW, ledW, 
+// array with all faces and all colors
+byte leds[] = {ledR, ledR, ledR, ledR, ledR, ledR, ledR, ledR, ledR,  // first face
+               ledW, ledW, ledW, ledW, ledW, ledW, ledW, ledW, ledW,  // second face
                ledB, ledB, ledB, ledB, ledB, ledB, ledB, ledB, ledB, 
                ledG, ledG, ledG, ledG, ledG, ledG, ledG, ledG, ledG, 
                ledY, ledY, ledY, ledY, ledY, ledY, ledY, ledY, ledY, 
-               ledV, ledV, ledV, ledV, ledV, ledV, ledV, ledV, ledV};
+               ledV, ledV, ledV, ledV, ledV, ledV, ledV, ledV, ledV}; // 6th face
 byte t0 = leds[45], t1 = leds[46], t2 = leds[47], t3 = leds[48], t4 = leds[49], t5 = leds[50], t6 = leds[51], t7 = leds[52], t8 = leds[53];
 byte b0 = leds[36], b1 = leds[37], b2 = leds[38], b3 = leds[39], b4 = leds[40], b5 = leds[41], b6 = leds[42], b7 = leds[43], b8 = leds[44];
 byte d0 = leds[27], d1 = leds[28], d2 = leds[29], d3 = leds[30], d4 = leds[31], d5 = leds[32], d6 = leds[33], d7 = leds[34], d8 = leds[35];
 byte l0 = leds[18], l1 = leds[19], l2 = leds[20], l3 = leds[21], l4 = leds[22], l5 = leds[23], l6 = leds[24], l7 = leds[25], l8 = leds[26];
 byte f0 = leds[9], f1 = leds[10], f2 = leds[11], f3 = leds[12], f4 = leds[13], f5 = leds[14], f6 = leds[15], f7 = leds[16], f8 = leds[17];
 byte r0 = leds[0], r1 = leds[1], r2 = leds[2], r3 = leds[3], r4 = leds[4], r5 = leds[5], r6 = leds[6], r7 = leds[7], r8 = leds[8];
+// buttons arduino pins
 const int bpin[] = {5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2};
+// waiting time
 int wait = 500;
 
 
 
 void setup(){
+  // setup for pins
   pinMode(datapin, OUTPUT);
   pinMode(clockpin, OUTPUT);  
   pinMode(latchpin, OUTPUT);
@@ -62,6 +67,8 @@ void loop(){
   for (int x = 0; x < 12; x++)
     b[x] = digitalRead(bpin[x]);
   
+  
+  // simulate movement
   if(b[0] == LOW) {
     //rotate left face CCW
     t0 = leds[45];
